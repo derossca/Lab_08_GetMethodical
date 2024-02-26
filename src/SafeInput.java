7e4aabea194aeeb9b6267ac158a4b40212595162
derossca <derossca@mail.uc.edu>
2024-02-26 12:50:43 -0500

    made method for RangedInt

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class SafeInput {
@@ -67,5 +68,36 @@ public class SafeInput {
         return retVal;
     }
 
+    //Part D getRangedInt
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
+    {
+        int retVal = 0;
+        String trash = "";
+        boolean done = false;
 
+        do
+        {
+            System.out.print("\n" + prompt + "[" + low + "-" + high + "]: ");
+            if(pipe.hasNextInt())
+            {
+                retVal = pipe.nextInt();
+                pipe.nextLine();
+                if(retVal >= low && retVal <= high)
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("\nNumber is out of range [" + low + "-" + high "]: " +retVal);
+                }
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter an int: " + trash);
+            }
+        } while(!done);
+
+        return retVal;
+    }
 }
