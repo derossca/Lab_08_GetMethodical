14c081b2871f926b38270600516736408d8461e7
derossca <derossca@mail.uc.edu>
2024-02-27 10:44:29 -0500

    made switch selectors to limit range depending on month

--- src/BirthDateTime.java
+++ src/BirthDateTime.java
@@ -14,11 +14,16 @@ public class BirthDateTime {
         //calling SafeInput getRangedInt methods for user prompt and input
         year = SafeInput.getRangedInt(in, "Enter the year you were born: ", 1950, 2015);
         month = SafeInput.getRangedInt(in, "Enter the month you were born: ", 1, 12);
-        day = SafeInput.getRangedInt(in, "Enter the day you were born: ", 1, 31);
-        switch (day)
+        switch (month)
         {
-            case 29:
-
+            case 2:
+                day = SafeInput.getRangedInt(in, "Enter the day you were born: ", 1, 29);
+                break;
+            case 1,3,5,7,8,10,12:
+                day = SafeInput.getRangedInt(in,"Enter the day you were born: ", 1,31);
+                break;
+            case 4,6,9,11:
+                day = SafeInput.getRangedInt(in,"Enter the day you were born: ", 1, 30);
         }
         hours = SafeInput.getRangedInt(in, "Enter the hour you were born: ", 1, 24);
         minutes = SafeInput.getRangedInt(in, "Enter the minute you were born: ", 1, 59);
