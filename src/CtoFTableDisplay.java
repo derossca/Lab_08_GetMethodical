4dd1894251edac046aec902c7d588e205b333ceb
derossca <derossca@mail.uc.edu>
2024-03-06 10:35:16 -0500

    made method to convert celsius to fahrenheit and loop in main method to display a table in CtoFTableDisplay java class

--- src/CtoFTableDisplay.java
+++ src/CtoFTableDisplay.java
@@ -5,6 +5,18 @@ public class CtoFTableDisplay
     public static void main(String[] args)
     {
 
+        System.out.println("Celsius     Fahrenheit");
+
+        for(double tempC = -100; tempC <= 100; tempC += 1)
+        {
+            System.out.printf("%5f\t%5f\n", tempC, cToF(tempC));
+        }
     }
 
+    private static double cToF(double tempC)
+    {
+        double tempF = (tempC * 9/5) + 32;
+
+        return tempF;
+    }
 }
